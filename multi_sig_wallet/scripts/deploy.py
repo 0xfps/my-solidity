f9eeba2377c7b58baffe497b20e61c477961dc0b
fps <anthony.nnaemeka.umeh@gmail.com>
2022-04-19 14:23:51 +0100

    Add files via upload

---
+++ multi_sig_wallet/scripts/deploy.py
@@ -0,0 +1,22 @@
+from brownie import MultiSigWallet, config, network, accounts
+
+def deploy():
+    print("Getting account....")
+    account = get_account()
+    print(f"Deployer account {account} confirmed!!!")
+    print("Deploying contract EtherWallet...")
+    deploy_wallet = MultiSigWallet.deploy({"from": account}, publish_source = True)
+    print(f"Contract deployed successfully at {deploy_wallet.address}")
+
+    with open("../Deployment Addresses.txt", "a+") as dep:
+        dep.write("\n\nMulti Sig Wallet => https://rinkeby.etherscan.io/address/"+deploy_wallet.address)
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config['wallet']['from_key'])
+
+
+def main():
+    deploy()
