41963bec26a308ff2fbe692f5b8e3a35f07eead4
fps <anthony.nnaemeka.umeh@gmail.com>
2022-04-21 22:01:00 +0100

    CrowdFund

---
+++ CrowdFund/contracts/README.md
@@ -0,0 +1,8 @@
+## Crowd Fund
+
+**Crowd fund ERC20 token**
+
+- User creates a campaign.
+- Users can pledge, transferring their token to a campaign.
+- After the campaign ends, campaign creator can claim the funds if total amount pledged is more than the campaign goal.
+- Otherwise, campaign did not reach it's goal, users can withdraw their pledge.
---
+++ CrowdFund/scripts/deploy.py
@@ -0,0 +1,23 @@
+from brownie import CrowdFund, config, network, accounts
+
+def deploy():
+    print("Getting account....")
+    account = get_account()
+    print(f"Deployer account {account} confirmed!!!")
+    print("Deploying contract CrowdFund...")
+    deploy_wallet = CrowdFund.deploy({"from": account}, publish_source = True)
+    print(f"Contract deployed successfully at {deploy_wallet.address}")
+
+    with open("../Deployment Addresses.txt", "a+") as dep:
+        dep.write("\n\nCrowdFund => "+deploy_wallet.address)
+
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config['wallet']['from_key'])
+
+
+def main():
+    deploy()
