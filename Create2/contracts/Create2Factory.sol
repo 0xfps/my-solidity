aacc0b5e84a8a4669631d8dc05cfc9f00bf6c327
Nnaemeka Umeh (fps) <anthony.nnaemeka.umeh@gmail.com>
2022-07-04 21:30:00 +0100

    Create Create2Factory.sol

---
+++ Create2/contracts/Create2Factory.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DeployWithCreate2 {
+    address public owner;
+    uint public f;
+
+    constructor (address _owner, uint e) {
+        owner = _owner;
+        f = e;
+    }
+}
+
+
+contract Create2Factory {
+    event Deploy(address addr);
+
+    function deploy(uint256 _salt) public {
+        DeployWithCreate2 _contract = new DeployWithCreate2 {
+            salt: bytes32(_salt)
+        }(msg.sender, 6);
+        emit Deploy(address(_contract));
+    }
+
+    function getAddress(bytes memory bytecode, uint256 _salt) public view returns(address) {
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                bytes1(0xff),
+                address(this),
+                _salt,
+                keccak256(bytecode)
+            )
+        );
+
+        return address(uint160(uint(hash)));
+    }
+
+    function getBytecode() public view returns(bytes memory bytecode) {
+        bytes memory cCode = type(DeployWithCreate2).creationCode;
+        bytecode = abi.encodePacked(cCode, abi.encode(msg.sender, 6));
+        // bytecode = abi.encodePacked(cCode, abi.encode(msg.sender), abi.encode(6));
+    }
+}
+
