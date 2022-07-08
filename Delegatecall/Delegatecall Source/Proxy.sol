17e64bb2be5b2b18908795879b34e88e403f4532
Nnaemeka Umeh (fps) <anthony.nnaemeka.umeh@gmail.com>
2022-07-08 19:38:45 +0100

    Update Proxy.sol

--- Delegatecall/Delegatecall Source/Proxy.sol
+++ Delegatecall/Delegatecall Source/Proxy.sol
@@ -1,30 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.6.0;
+
 import "./Utils/Store.sol";
 import "./Utils/Admin.sol";
 
-/*
- * @title: 
- * @author: Anthony (fps) https://github.com/fps8k .
- * @dev: 
-*/
 
-contract Proxy is Store, Admin
-{
-    function add(uint _a, uint _b) public
-    {
+/*
+ * @title: Delegatecall Proxy.
+ * @author: Anthony (fps) https://github.com/fps8k.
+ * @dev:
+*/
+contract Proxy is Store, Admin {
+    function add(uint _a, uint _b) public {
         address cont = current_contract;
-        (bool sent, ) = cont.delegatecall(abi.encodeWithSignature("add(uint256,uint256)", _a, _b));
+        (bool sent, ) = cont.delegatecall(
+            abi.encodeWithSignature(
+                "add(uint256,uint256)", 
+                _a, 
+                _b
+            )
+        );
         require(sent, "Delegatecall, failed");
     }
 
-    function getTotal() public view returns(uint)
-    {
+    function getTotal() public view returns(uint) {
         return total;
     }
 
-    function gt() public view returns(address)
-    {
+    function gt() public view returns(address) {
         return current_contract;
     }
 }
