15bbce908ee162aec74b03cc079cc83442d6149f
Nnaemeka Umeh (fps) <anthony.nnaemeka.umeh@gmail.com>
2022-07-08 19:37:05 +0100

    Update Main.sol

--- Delegatecall/Delegatecall Deployed/contracts/Utils/Main.sol
+++ Delegatecall/Delegatecall Deployed/contracts/Utils/Main.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.6.0;
+
 import "./Store.sol";
 
+
 /*
- * @title: 
- * @author: Anthony (fps) https://github.com/fps8k .
+ * @title: Delegatecall Main.
+ * @author: Anthony (fps) https://github.com/fps8k.
  * @dev: 
 */
-
-contract Main is Store
-{
-    function add(uint _a, uint _b) public
-    {
+contract Main is Store {
+    function add(uint _a, uint _b) public {
         total = _a + _b;
     }
 }
