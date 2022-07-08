e2aeffa68f4b33775474d765271f52853790dcfa
Nnaemeka Umeh (fps) <anthony.nnaemeka.umeh@gmail.com>
2022-07-08 19:41:54 +0100

    Update Store.sol

--- Delegatecall/Delegatecall Source/Utils/Store.sol
+++ Delegatecall/Delegatecall Source/Utils/Store.sol
@@ -2,12 +2,10 @@
 pragma solidity >0.6.0;
 
 /*
- * @title: 
- * @author: Anthony (fps) https://github.com/fps8k .
- * @dev: 
+ * @title: Delegatecall Store.
+ * @author: Anthony (fps) https://github.com/fps8k.
+ * @dev: Storage varaiable contract.
 */
-
-contract Store
-{
+contract Store {
     uint public total;
 }
