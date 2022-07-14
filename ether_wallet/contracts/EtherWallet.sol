8653e81f49e4a4f806616d99c36e032a72cc466f
Nnaemeka Umeh (fps) <anthony.nnaemeka.umeh@gmail.com>
2022-07-14 07:07:27 +0100

    Update EtherWallet.sol

--- ether_wallet/contracts/EtherWallet.sol
+++ ether_wallet/contracts/EtherWallet.sol
@@ -7,73 +7,47 @@ pragma solidity >0.6.0;
  * @dev: Ether Wallet: https://github.com/fps8k/my-solidity/tree/main/ether_wallet
  * This is a simple Solidity program that allows anyone to send ether to a wallet then allowing only the owner to withdraw.
 */
-
-contract EtherWallet
-{
+contract EtherWallet {
     // Declare owner, deploy time, ended, and locking.
-
     address owner;
     uint256 deploy_time;
     bool ended;
     bool locked;
 
-
     // Events.
-
     event Deployed(address, uint256);           // owner, deploy_time.
     event Funded(address, uint256);             // msg.sender, msg.value.
     event Withdraw(address, uint256);           // owner, address(this).balance.
 
-
-    constructor()
-    {
+    constructor() {
         owner = msg.sender;             // Set the owner of the contract to the deployer.
         deploy_time = block.timestamp;  // Set time of deployment.
         ended = false;                  // Ended to control the funding.
-
         emit Deployed(owner, deploy_time);
     }
 
-
-
-
-    modifier NoReentrance()
-    {
+    modifier NoReentrance() {
         require(!locked, "You cannot redo this action.");
         locked = true;
         _;
         locked = false;
     }
 
-
-
-
-    function fund() public payable NoReentrance
-    {
+    function fund() public payable NoReentrance {
         require(msg.sender != address(0), "!Address");
         require(msg.sender != owner, "Owner !Send");
-
         emit Funded(msg.sender, msg.value);
     }
 
-
-
-
-    function viewFunds() public view returns(uint256 balance)
-    {
+    function viewFunds() public view returns(uint256 balance) {
         require(msg.sender == owner, "!Owner");
         balance = address(this).balance;
     }
 
-
-
-
-    function withdrawFunds() public payable NoReentrance
-    {
+    function withdrawFunds() public payable NoReentrance {
         require(msg.sender == owner, "!Owner");
         uint256 bal = address(this).balance;
         payable(owner).transfer(address(this).balance);
-        
         emit Withdraw(msg.sender, bal);
     }
 }
