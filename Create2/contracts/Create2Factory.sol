d6ff1538228f464e2dedaec49399cdbd5a957811
fps <anthony.nnaemeka.umeh@gmail.com>
2022-07-27 17:24:05 +0100

    Update Create2Factory.sol

--- Create2/contracts/Create2Factory.sol
+++ Create2/contracts/Create2Factory.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// @dev Contract to be deployed with Create2.
 contract DeployWithCreate2 {
     address public owner;
     uint public f;
@@ -15,11 +16,23 @@ contract DeployWithCreate2 {
     }
 }
 
-
+/// @dev Contract deploying with Create2.
 contract Create2Factory {
     event Deploy(address addr);
     address public _add;
 
+    /**
+    * @dev  Deploy using any salt of user choice.
+    *       When deployed with this function using a
+    *       particular salt, and that same salt is used
+    *       again, this throws an error.
+    *       Also, when deployed with this function using 
+    *       a particular salt, and that same salt is used
+    *       in the deployWithAssemblyCreate2, it also 
+    *       throws an error.
+    *
+    * @param _salt A random uint256.
+    */
     function deploy(uint256 _salt) public {
         DeployWithCreate2 _contract = new DeployWithCreate2 {
             salt: bytes32(_salt)
@@ -27,6 +40,16 @@ contract Create2Factory {
         emit Deploy(address(_contract));
     }
 
+    /**
+    * @dev  Returns the pre-calculated address of the contract
+    *       to be deployed using Create2.
+    *       
+    * @param bytecode   Compiled bytecode of contract to be 
+    *                   deployed.
+    * @param _salt      A random uint256.
+    *
+    * @return address Pre-calculated address.
+    */
     function getAddress(bytes memory bytecode, uint256 _salt) public view returns(address) {
         bytes32 hash = keccak256(
             abi.encodePacked(
@@ -40,18 +63,42 @@ contract Create2Factory {
         return address(uint160(uint(hash)));
     }
 
+    /**
+    * @dev  Returns the bytecode of the contract to be deployed
+    *       along with its constructor parameters.
+    *
+    * @return bytecode Contract bytecode.
+    */
     function getBytecode() public view returns(bytes memory bytecode) {
         bytes memory cCode = type(DeployWithCreate2).creationCode;
         bytecode = abi.encodePacked(cCode, abi.encode(msg.sender, 6));
         // bytecode = abi.encodePacked(cCode, abi.encode(msg.sender), abi.encode(6));
     }
 
-    /// @dev    Deploys with assembly create 2.
-    ///         You cannot deploy 2 contracts with the same salt.
-    ///         Running this function a second time with the same salt,
-    ///         overrides the address stored at `_add` with a 0 address
-    ///         and cannot be redeployed.
-    function deployWithAssemblyUsingBytes32(uint256 _salt) public {
+    /** @dev    Deploys with assembly create 2.
+    *           THIS IS THE PERK:
+    *           When this function is called first time,
+    *           the contract is deployed successfully,
+    *           returning and storing the address of the 
+    *           deployed contract to `_add`.
+    *           Calling the deploy function above will throw
+    *           an error.
+    *
+    *           However, after this function is called first
+    *           time and the above happens, storing the address
+    *           in the `_add` variable, when it is called again,
+    *           the transaction is successful, meaning a new 
+    *           contract is deployed, but now, the new address
+    *           and the addresses of other subsequent deploys is
+    *           now a 0 address. Hence, the address in the `_add`
+    *           variable is replaced with a 0 address.
+    *
+    * @notice   Address after first deployement with _salt as 6:
+    *           0x9138AcCfF44091B80C53F35678188f90dc5494fb.
+    *           Address after calling the function a second time:
+    *           0x0000000000000000000000000000000000000000.
+    */
+    function deployWithAssemblyCreate2(uint256 _salt) public {
         bytes memory cCode = type(DeployWithCreate2).creationCode;
         bytes memory bytecode = abi.encodePacked(cCode, abi.encode(msg.sender, 6));
         bytes32 salt = bytes32(_salt);
@@ -62,6 +109,7 @@ contract Create2Factory {
         _add = _address;
     }
 
+    /// @dev Destruct the deployed contract.
     function destroy(address _a) public {
         DeployWithCreate2(_a).selfDestruct();
         _add = address(0);
