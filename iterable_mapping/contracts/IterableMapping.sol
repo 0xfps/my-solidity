f9632399995e22d86b3e47c50c34c4e9709fcce7
fps <anthony.nnaemeka.umeh@gmail.com>
2022-08-06 07:51:40 +0100

    Update IterableMapping.sol

--- iterable_mapping/contracts/IterableMapping.sol
+++ iterable_mapping/contracts/IterableMapping.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.6.0;
 
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.14;
-
 /**
 * @title Iterable Mapping.
 * @author Anthony (fps) https://github.com/0xfps.
